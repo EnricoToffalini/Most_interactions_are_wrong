39b8faba55a4defe154fce62917fc5ee807d7182
EnricoToffalini <enrico.toffalini@unipd.it>
2025-04-07 11:03:36 +0200

    update

--- Notes/Other_notes/aggregateRT.R
+++ Notes/Other_notes/aggregateRT.R
@@ -4,6 +4,7 @@
 library(tidyr)
 library(dplyr)
 library(glmmTMB)
+library(effects)
 
 set.seed(0)
 
@@ -30,15 +31,20 @@ df$rInt = rInt[df$subject]
 
 df$response = rgamma(nrow(df), rate = 1/exp(1 + df$A*0.6 + df$B*0.8 + df$rInt), shape=10)
 hist(df$response)
+df$A = as.factor(df$A)
+df$B = as.factor(df$B)
 
 ########################################
 
 fit = glmmTMB(response ~ A*B+(1|subject), family=Gamma(link="log"), data=df)
 summary(fit)
+plot(allEffects(fit),multiline=T)
 
 dfx = df
-dfx$response = log(dfx$response)
-dfx = aggregate(dfx,by=list(df$subject,df$A,df$B),FUN=mean)
+#dfx$response = log(dfx$response)
+dfx$A = as.integer(dfx$A)-1
+dfx$B = as.integer(dfx$B)-1
+dfx = aggregate(dfx,by=list(df$subject,df$A,df$B),FUN=median)
 
 fit = lm(response ~ A*B, data=dfx)
 summary(fit)
